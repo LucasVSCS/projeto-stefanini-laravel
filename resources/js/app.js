e48f547a473bfa2ceb38e47ed080b3c0d9536303
Lucas Vinicius <lucasvscs@outlook.com>
2021-03-06 23:49:24 -0300

    Atualizando os imports padrões do laravel mix do JS e do CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,10 @@
 require('./bootstrap')
+require('./cep-api')
+import Inputmask from 'inputmask'
 import 'bootstrap'
+
+// Gerando e exibindo o ano atual no footer da página
+document.getElementById('yearNow').textContent = new Date().getFullYear()
+
+// Implementando a máscara do input "CEP" no carrinho de compras
+Inputmask('99999-999').mask(document.getElementById('zip'))
